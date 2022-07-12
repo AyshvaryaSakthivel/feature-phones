67ad2692f7315c6f89e3142c20e60d1104b7bf98
AyshvaryaSakthivel <88436866+AyshvaryaSakthivel@users.noreply.github.com>
2022-07-12 19:49:17 +0530

    Delete src directory

--- src/main/java/com/gupshup/feature/phones/service/ReceiveRequest.java
+++
@@ -1,8 +0,0 @@
-package com.gupshup.feature.phones.service;
-
-public interface ReceiveRequest {
-
-	int make_request(byte[] input_buffer, int input_buffer_len, byte[] cmd_identifier, int cmd_identifier_len,
-			byte[] output_buffer);
-
-}
--- src/main/java/com/gupshup/feature/phones/service/ReceiveService.java
+++
@@ -1,8 +0,0 @@
-package com.gupshup.feature.phones.service;
-
-public interface ReceiveService {
-
-	public int parseRequestAtReceiverSide(byte[] input_buffer, int length, byte[] output_buffer, byte[] request_buffer,
-			int output_length);
-
-}
